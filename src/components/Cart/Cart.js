9505a4e0172cb6f8515edf700da2ab3d2c28d832
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-04-20 13:41:40 +0700

    Membuat component cart, & reusable component modal, memberi modal style

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+// import Card from '../UI/Card';
+// import classes from './Cart.module.css';
+
+const Cart = () => {
+    return (
+        <div>
+            <p>Hello Gaes!</p>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,35 @@
+import { createPortal } from 'react-dom';
+import { Fragment } from 'react';
+
+import Card from './Card';
+import classes from './Modal.module.css';
+
+
+const Background = props => {
+    return (
+        <div className={classes.background}></div>
+    );
+}
+
+
+const ModalOverlays = props => {
+    return (
+        <Card className={classes['modal-overlays']}>
+            {props.children}
+        </Card>
+    );
+}
+
+
+const portals = document.getElementById('modal');
+
+const Modal = props => {
+    return (
+        <Fragment>
+            {createPortal(<Background />, portals)}
+            {createPortal(<ModalOverlays>{props.children}</ModalOverlays>, portals)}
+        </Fragment>
+    );
+}
+ 
+export default Modal;
