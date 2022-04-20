56cdb7b685ff3b4687dcdb589918d0cc94d9a926
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-04-20 19:04:56 +0700

    membuat jumlah harga menjadi dinamis, hasil dari semua orderedmeals

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,6 +11,8 @@ const Cart = props => {
     const orderedMeals = useSelector(state => state.orderedMeals.orderedMeals);
     const dispatch = useDispatch();
 
+    const totalAmount = orderedMeals.map(meal => parseInt(meal.totalPrice)).reduce((acc, curr) => acc + curr, 0);
+
     return (
         <Modal className={classes.cart}>
             <ul>
@@ -25,7 +27,7 @@ const Cart = props => {
             </ul>
             <div className={classes.total}>
                 <h2>Total Amount</h2>
-                <h2>$34000</h2>
+                <h2>${totalAmount.toFixed(2)}</h2>
             </div>
             <div className={classes.btn}>
                 <Button onClick={() => dispatch(setModalCart(false))}>Close</Button>
